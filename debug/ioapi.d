b79d36fa2934c20288530afe55d4c7808e0dcdc8
Régis LIMARE <regis.limare@opcoep.fr>
2026-09-07 10:10:46 +0200

    revert delete gitignore

--- debug/ioapi.d
+++
@@ -1,9 +0,0 @@
-ioapi.o: /project/zlib/contrib/minizip/ioapi.c \
- /project/zlib/contrib/minizip/ioapi.h \
- /opt/devkitpro/portlibs/ppc/include/zlib.h \
- /opt/devkitpro/portlibs/ppc/include/zconf.h \
- /project/zlib/contrib/minizip/ints.h
-/project/zlib/contrib/minizip/ioapi.h:
-/opt/devkitpro/portlibs/ppc/include/zlib.h:
-/opt/devkitpro/portlibs/ppc/include/zconf.h:
-/project/zlib/contrib/minizip/ints.h:
